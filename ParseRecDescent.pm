bf026467f8e94d204f2694feb10cdc511dee7ad9
Timm Murray <tmurray@doubleprime.com>
2012-08-22 20:13:00 -0500

    Implement Parse::RecDescent version

--- ParseRecDescent.pm
+++ ParseRecDescent.pm
@@ -2,11 +2,40 @@ package ParseRecDescent;
 use v5.14;
 use Parse::RecDescent;
 
+my $GRAMMAR = <<'END_GRAMMAR';
+    startrule: func
+
+    func: open_paren op arg(s?) close_paren
+        {
+            $return = [
+                $item[2],
+                @{ $item[3] },
+            ];
+        }
+
+    open_paren: '('
+    close_paren: ')'
+
+    op: '+'
+    op: '-'
+    op: '*'
+    op: '/'
+
+    arg: integer
+    arg: func
+
+    integer: /\d+/
+END_GRAMMAR
+
 
 sub new
 {
     my ($class) = @_;
-    my $self = {};
+    my $parser = Parse::RecDescent->new( $GRAMMAR );
+
+    my $self = {
+        parser => $parser,
+    };
     bless $self => $class;
 }
 
@@ -14,7 +43,10 @@ sub new
 sub parse
 {
     my ($self, $text) = @_;
-    return ['+', 1];
+    my $parser = $$self{parser};
+
+    my $ast = $parser->startrule( $text );
+    return $ast;
 }
 
 
